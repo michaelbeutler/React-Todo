9ac9d703590101c95fa652b6fb1aca9c0ba0c5cc
MichiBeutler <michael.beutler@gibmit.ch>
2019-10-23 16:07:37 +0200

    added testing cases to todoitem component

--- src/components/TodoItem.test.js
+++ src/components/TodoItem.test.js
@@ -9,15 +9,39 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TodoItem id={1} text={"Test todo"} />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
-
 it('checkbox is clickable and state updates', () => {
     const item = shallow(<TodoItem id={1} text={"Test todo"} />);
     expect(item.find('label').text()).toEqual('Test todo');
     item.find('input').simulate('change');
     expect(item.state('completed')).toBe(true);
+});
+
+it('checkbox value equals props (completed=true)', () => {
+    const item = shallow(<TodoItem id={1} completed={true} text={"Test todo"} />);
+    expect(item.state('completed')).toBe(true);
+});
+
+it('checkbox value equals props (completed=false)', () => {
+    const item = shallow(<TodoItem id={1} completed={false} text={"Test todo"} />);
+    expect(item.state('completed')).toBe(false);
+});
+
+it('checkbox value equals props (completed=undefined)', () => {
+    const item = shallow(<TodoItem id={1} text={"Test todo"} />);
+    expect(item.state('completed')).toBe(false);
+});
+
+it('label style equals props (completed=true)', () => {
+    const item = shallow(<TodoItem id={1} completed={true} text={"Test todo"} />);
+    expect(item.find('label').props('style').style.textDecoration).toEqual('line-through');
+});
+
+it('label style equals props (completed=false)', () => {
+    const item = shallow(<TodoItem id={1} completed={false} text={"Test todo"} />);
+    expect(item.find('label').props('style').style.textDecoration).toEqual('none');
+});
+
+it('label style equals props (completed=undefined)', () => {
+    const item = shallow(<TodoItem id={1} text={"Test todo"} />);
+    expect(item.find('label').props('style').style.textDecoration).toEqual('none');
 });
